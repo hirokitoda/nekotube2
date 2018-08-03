6bbd51eb84945e0ba71773d02f537e0f3e162f22
hirokitoda <hirokimusic123@gmail.com>
2018-08-03 16:32:01 +0900

    youtube_APIkey

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import Ysearch from 'youtube-api-search';
+
+const YOUTUBE_API_KEY = 'AIzaSyAypMwkSgPExT_Sm1e6ckqlgG8GqysFxkY'
+
+Ysearch({key: YOUTUBE_API_KEY, term: '猫　きゅうり'}, (data) => {
+  console.log(data);
+});
 
 class App extends Component {
   render() {
