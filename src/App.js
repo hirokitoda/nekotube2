ab981aa7db3f85240994ac799658647f519327fa
hirokitoda <hirokimusic123@gmail.com>
2018-08-03 19:21:59 +0900

    note11

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import Ysearch from 'youtube-api-search';
 import Body from './components/Body/Body';
 import List from './components/Video/List/List';
+import Video from './components/Video/Video';
 
 const YOUTUBE_API_KEY = 'AIzaSyAypMwkSgPExT_Sm1e6ckqlgG8GqysFxkY'
 
@@ -21,7 +22,10 @@ componentDidMount(){
     return (
       <div className="App">
         <Header />
-        <Body><List videos={this.state.videos} /></Body>
+        <Body>
+          <Video video={this.state.videos[1]} />
+          <List videos={this.state.videos} />
+        </Body>
       </div>
     );
   }
